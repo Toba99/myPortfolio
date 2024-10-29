eb7ae0244e094bc0311b28eec3493299c9bfaa06
Martins Eweniyi <oloruntobaeweniyi@gmail.com>
2024-10-29 16:54:54 +0000

    new

--- pages/index.js
+++ pages/index.js
@@ -36,7 +36,7 @@ export default function Home() {
               <li>
                 <a
                   className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
-                  href="/Eweniyi_Oloruntoba_Martins_CV.pdf"
+                  href="/Eweniyi_Oloruntoba_Martins-CV.pdf"
                 >
                   Resume
                 </a>
